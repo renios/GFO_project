ff045e2cd6ffd66ea46612f28e5f5b34f483cab2
sdsoso <soso_melon@naver.com>
2017-05-28 08:27:17 +0900

    ButtonMove(bata)
    아직 미완성 이므로 오류가 많음

---
+++ Assets/c#/UIButtonManger.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIButtonManger : MonoBehaviour
+{ 
+    
+	public void LeftClick ()
+    {
+
+    }
+
+    public void RightClick ()
+    {
+        
+    }
+
+    public void JumpClick ()
+    {
+  
+    }
+}
