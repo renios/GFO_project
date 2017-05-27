83dd725e5e806f1d736bf30b97470a8724d81286
hihi6833 <hihi6833@naver.com>
2017-05-27 16:39:37 +0900

    Merge branch 'master' of https://github.com/renios/GFO_project

--- Assets/Script/storytest.cs
+++ Assets/Script/storytest.cs
@@ -16,21 +16,25 @@ public class storytest : MonoBehaviour {
 	void Update () {
 		if (num ==0)
 		{
-			textObject.text = "아들아 우리의 해가 서쪽으로 지고 있다. 왕에게문제가 있는 것 같구나. 마을의 건장한 청년은 모두 끌려가 너희 형마저 전쟁터에 갔으니 왕에게 무슨일이 있는지 알 도리가 없구나. 네가 가야겠다. ";
+			textObject.text = "아들아 우리의 해가 서쪽으로 지고 있다. 왕에게 문제가 있는 것 같구나.";
 		}
 		else if (num == 1)
 		{
+			textObject.text = "마을의 건장한 청년은 모두 끌려가 너희 형마저 전쟁터에 갔으니 왕에게 무슨일이 있는지 알 도리가 없구나. 네가 가야겠다. ";
+		}
+		else if (num == 2)
+		{
 			textObject.text = "네 어머니.";
 		}	
-		else if  (num == 2)
+		else if  (num == 3)
 		{
 			textObject.text = "아들아!!!!";
 		}
-		else if (num == 3)
+		else if (num == 4)
 		{
 			textObject.text = "?";
 		}
-		else if (num == 4)
+		else if (num == 5)
 		{
 			textObject.text = "돌아가신 너희 아빠가 남긴 검이다. 상처가 생긴다면 이 물약을 먹거라.";
 		}
