5fdd05b0dcc70ade1face4e684cd15764c01a26e
sdsoso <soso_melon@naver.com>
2017-06-03 17:38:21 +0900

    ....

---
+++ Assets/c#/PotalManager.cs
@@ -0,0 +1,26 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PotalManager : MonoBehaviour {
+
+    public string type;
+    public float value = 0;
+    public GameObject portal;
+    Rigidbody2D rigid;
+
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.layer == 9 && rigid.velocity.y < 0)
+        {
+
+            BlockStatus block = other.gameObject.GetComponent<BlockStatus>
+                Debug.Log(block.type) {
+
+            }
+        }
+    }
+
+}
