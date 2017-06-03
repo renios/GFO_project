18d8692ad43f87b1928481ebda87241cf1574c19
sdsoso <soso_melon@naver.com>
2017-06-03 15:10:58 +0900

    .

---
+++ Assets/c#/health.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class health : MonoBehaviour {
+	public int player_helath;
+	public Text textobject;
+	
+
+	// Use this for initialization
+	void Start () {
+		player_helath = 30;
+		textobject.text = "채력:" + player_helath;
+
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.O)){
+			Damaged(1);
+			// player_helath--;
+			textobject.text = "채력:" + player_helath;
+		}
+	}
+
+	void Damaged(int damage)
+	{
+		
+
+	}
+}
