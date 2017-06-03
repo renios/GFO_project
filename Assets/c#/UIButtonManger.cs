d11121957c5d4de5a2aa1f49b40734d62a8344a7
sdsoso <soso_melon@naver.com>
2017-06-03 13:49:10 +0900

    땅

--- Assets/c#/UIButtonManger.cs
+++ Assets/c#/UIButtonManger.cs
@@ -3,42 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class UIButtonManger : MonoBehaviour {
-    GameObject player;
-    move_3 playerScript;
 
-    public void Init()
-    {
-        player = GameObject.FindGameObjectWithTag("player");
-        playerScript = player.GetComponent<move_3>();
-    }
-
-    public void LeftDown()
-    {
-        playerScript.inputLeft = true;
-    }
-    public void LeftUp()
-    {
-        playerScript.inputLeft = false;
-    }
-    public void RightDown()
-    {
-        playerScript.inputRight = true;
-    }
-    public void RightUp()
-    {
-        playerScript.inputRight = false;
-    }
-    public  void LeftClick()
-    {
-        Debug.Log("왼쪽버튼 누름");
-    }
-    public void RightClick()
-    {
-        Debug.Log("오른쪽 버튼 누름");
-    }
-    public void JumpClick()
-    {
-        Debug.Log("점프 버튼 누름");
-    }
-
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
 }
