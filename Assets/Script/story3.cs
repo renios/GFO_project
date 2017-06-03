b1766999c0aa2a18b63dab4e2ecacfe824a12982
hihi6833 <hihi6833@naver.com>
2017-06-03 14:56:16 +0900

    스테이지 4까지 대화 수정

---
+++ Assets/Script/story3.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class story3 : MonoBehaviour {
+
+	public Text textObject;	
+	public int num;
+	// Use this for initialization
+	void Start () {
+		num = 9;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (num == 9)
+		{
+			textObject.text = "우린 너희를 살리려 했을 뿐이다. ";
+		}
+		else 
+		{
+			SceneManager.LoadScene("4");
+		}
+	}
+
+	public void Click (){
+		num = num+1;
+	}
+}
