d2dfa45eb868d8f9c393301254458af65c943931
Minjung Kim <Reny.atkins@gmail.com>
2017-06-08 23:35:58 +0900

    조이패드 구현

--- Assets/c#/move_1.cs
+++ Assets/c#/move_1.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class move_1 : MonoBehaviour {
 	public int Speed;
 	bool leftButtonClicked = false;
+	bool rightButtonClicked = false;
 
 	// Use this for initialization
 	void Start () {
@@ -13,17 +14,32 @@ public class move_1 : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if(Input.GetKey (KeyCode.D) || leftButtonClicked) {
-			transform.Translate (Vector3.right * Speed *Time.deltaTime);
+		if(Input.GetKey (KeyCode.D) || rightButtonClicked) {
+			transform.Translate (Vector3.right * Speed * Time.deltaTime);
 		}
-			if(Input.GetKey (KeyCode.A)) {
-			transform.Translate (Vector3.left * Speed *Time.deltaTime);
+		if(Input.GetKey (KeyCode.A) || leftButtonClicked) {
+			transform.Translate (Vector3.left * Speed * Time.deltaTime);
 		}
 		
 	}
 
-	public void MoveLeft()
+	public void MoveLeftButtonDown()
 	{
 		leftButtonClicked = true;
+	}
+
+	public void MoveLeftButtonUp()
+	{
+		leftButtonClicked = false;
+	}
+
+	public void MoveRightButtonDown()
+	{
+		rightButtonClicked = true;
+	}
+
+	public void MoveRightButtonUp()
+	{
+		rightButtonClicked = false;
 	}
 }
