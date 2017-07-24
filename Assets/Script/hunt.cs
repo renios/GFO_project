ff1b5b51647d1d08d845e1e7516d1dadfe2971ea
hihi6833 <hihi6833@naver.com>
2017-07-24 23:27:55 +0900

    사냥터-스크립트 수정바람-스크립트 이름 hunt

---
+++ Assets/Script/hunt.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class hunt : MonoBehaviour 
+{
+
+	// Use this for initialization
+	public int num;
+	void Start () 
+	{
+		num = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (num == 10) 
+		{
+			SceneManager.LoadScene ("hunting");
+		}
+	}
+	public void Click ()
+	{
+		num = num + 1;
+	}
+}
